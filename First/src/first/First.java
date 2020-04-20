eb8e0815624790dc895889155002183e1d34e2c2
drb372 <daudbutt400@gmail.com>
2020-04-20 16:12:12 +0500

    keep Records

---
+++ First/src/first/First.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package first;
+
+import logins.Third_PaGe;
+import logins.login;
+
+/**
+ *
+ * @author Dell
+ */
+public class First {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        logins.login L1 = new login();
+        L1.setVisible(true);
+        // TODO code application logic here
+       
+    }
+    
+}
